7fbae5fa0ec1efbe10f7ccecaae9d196fb472d53
Khushmita Kaur <khushp9894@gmail.com>
2024-01-20 21:57:28 -0800

    Adding Comments/Intuition

--- neetcode/217.py
+++ neetcode/217.py
@@ -1,3 +1,18 @@
+"""
+arr1 = [1,2,3,4]; False
+arr2 = [1,2,3,2]; True
+arr2 = []; False
+arr4 = [1]; False
+
+We will take the element in the first index.
+Compare it to the rest of the elements in the array using for loops.
+
+
+Time Comp: o(n^2)
+Space Comp: O(1)
+
+"""
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
