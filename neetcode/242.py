2cdd22414683395608ac313956160a527004c8ec
Khushmita Kaur <khushp9894@gmail.com>
2024-01-22 23:15:34 -0800

    Added method 2

--- neetcode/242.py
+++ neetcode/242.py
@@ -15,3 +15,16 @@ class Solution:
         sorted_stringT = sorted()
         
         return sorted_stringS == sorted_stringT
+
+"""
+METHOD 2: Create hashmaps for both strings. The keys will be the characters and values will be the occurence of each character.
+The characters and counts are stored in hashmaps for both strings.
+The items in first string are compared to the second string items.
+If they are the same, we have anagrams, return true.
+Otherwise, return false.
+
+Time Complexity : O(n)
+Space Complexity : O(n)
+
+""" 
+        
