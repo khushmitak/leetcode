e5fdc78cc3a31b801f611e41eb40fc3f1ec2284d
Khushmita Kaur <khushp9894@gmail.com>
2024-01-22 22:56:00 -0800

    Added intuition for sorting method

--- neetcode/242.py
+++ neetcode/242.py
@@ -1,3 +1,14 @@
+"""
+METHOD 1: Both strings s and t can be sorted to check if they are anagrams.
+Sorting will arrange the strings in ascending order.
+Then, if the two sorted strings are the same, they are anagrams, return true.
+Otherwise, return false.
+
+Time Complexity : O(n log n)
+Space Complexity : O(1)
+
+"""
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         sorted_stringS = sorted()
