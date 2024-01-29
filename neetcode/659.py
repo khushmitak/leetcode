f35dae70abb6e2298bb40348ca58643f7a19f2a6
Khushmita Kaur <khushp9894@gmail.com>
2024-01-28 23:18:39 -0800

    add decode function

--- neetcode/659.py
+++ neetcode/659.py
@@ -5,3 +5,18 @@ class Solution:
         for s in strs:
             result += str(len(s)) + "#" + s
         return result
+
+    def decode(self, str):
+        result = []
+        i = 0
+        
+        while i < len(str):
+            j = i
+            while str[j] != "#":
+                j += 1
+            length = int(str[i:j])
+            result.append(str[j + 1: j + 1 + length])
+            i = j + 1 + length
+        return result
+
+        
