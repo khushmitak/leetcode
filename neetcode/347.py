38dbe2bad084f24f261f875f1bf2b8a8905d91c9
Khushmita Kaur <khushp9894@gmail.com>
2024-01-30 23:06:58 -0800

    add solution

--- neetcode/347.py
+++ neetcode/347.py
@@ -1,3 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        array = [[] for i in range(len(nums) + 1)]
         
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        
+        for n, c in count.items():
+            array[c].append(n)
+        
+        result = []
+        for i in range(len(array) - 1, 0, -1):
+            for n in array[i]:
+                result.append(n)
+                if (len(result)) == k:
+                    return result
