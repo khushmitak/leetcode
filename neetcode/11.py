a1c5f63b64b04060ffddd4088b4fd4ecf3c777a2
Khushmita Kaur <khushp9894@gmail.com>
2024-02-02 23:17:20 -0800

    add brute force

---
+++ neetcode/11.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        #brute force
+        
+        result = 0
+        
+        for l in range(len(height)):
+            for r in range(l + 1, len(height)):
+                area = (r - l) * min(height[l], height[r])
+                result = max(result, area)
+                
+        return result
