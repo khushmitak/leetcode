3f3be7ea0b3365232bb31facfca085f21115586f
Khushmita Kaur <khushp9894@gmail.com>
2024-02-05 09:14:43 -0800

    add comments

--- neetcode/20.py
+++ neetcode/20.py
@@ -6,12 +6,14 @@ class Solution:
         closeToOpen = {")" : "(", "]" : "[", "}" : "{"}
         
         for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
+            if c in closeToOpen: #check if c is a closing parenthesis
+                #check if stack is not empty and top element is opening parenthesis for c
+                if stack and stack[-1] == closeToOpen[c]: 
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
-        
+                
+        # check if the stack is empty indicating that the stack is valid
         return True if not stack else False
