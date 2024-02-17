1362539b904a6ce8ab70124fea094241617b713d
Khushmita Kaur <khushp9894@gmail.com>
2024-02-16 18:13:20 -0800

    add intuition

--- neetcode/36.py
+++ neetcode/36.py
@@ -1,3 +1,8 @@
+"""METHOD: To check if the sudoku is valid, check if the element at the current row, column index does not already
+exist in that particular row, column and 3 * 3 square, if it does -- the sudoku is not valid, return false.
+If the element is not present in the set already, then add that element to row, column and square to keep the sudoku updated.
+
+"""
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = collections.defaultdict(set)
